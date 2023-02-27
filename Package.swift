2876cbed36e0a1f814f6b191b5168ba74013cb36
minsook-smartwave <79209034+minsook-smartwave@users.noreply.github.com>
2023-02-27 10:12:54 -0500

    Update Package.swift
    Updated package information

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    products: [
+    .library(
+        name: "Locksmith",
+        targets: ["Locksmith"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Locksmith",
+            path: "Source"),
+    ]
 )
